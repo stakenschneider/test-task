a17aeec8c4663a562aab56f3c6691d23ab79a93a
stakenschneider <volkova.maria.d@gmail.com>
2020-07-23 22:34:09 +0300

    small changes

--- src/client/main/components/mainContent/MainContent.js
+++ src/client/main/components/mainContent/MainContent.js
@@ -3,6 +3,7 @@ import TopBar from "../topBar/TopBar";
 import AviaryInfoCard from "../aviaryInfoCard/AviaryInfoCard";
 import GiraffeList from "../giraffeList/GiraffeList";
 import '../styles.css'
+import UpdateCard from "../updatesCard/UpdatesCard";
 
 export default function MainContent() {
 
@@ -10,6 +11,7 @@ export default function MainContent() {
         <div className='content-background'>
             <TopBar/>
             <GiraffeList/>
+            <UpdateCard/>
             <AviaryInfoCard/>
         </div>
     );
